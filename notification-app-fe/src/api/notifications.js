e324e1eac94f1e315af1c1d82027503428ab1bd4
Harikrishnan S <harikrishnan3522@gmail.com>
2026-06-30 12:48:58 +0530

    Notification api written

--- notification-app-fe/src/api/notifications.js
+++ notification-app-fe/src/api/notifications.js
@@ -1,2 +1,13 @@
+
+
 export function fetchNotifications() {
+    fetch('http://4.224.186.213/evaluation-service/notifications', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => console.log(data));  
 }
